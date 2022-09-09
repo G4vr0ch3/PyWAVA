bbe4b844c1a555029f8795fcf05d9cbc958f69a1
Gavroche <arthur.reppelin@gmail.com>
2022-09-09 17:45:35 +0200

    licensing

--- libs/defender.py
+++ libs/defender.py
@@ -1,4 +1,37 @@
+#!/usr/bin/python3.10
+
+
+################################################################################
+#                                                                              #
+#                                                                              #
+#                   GNU AFFERO GENERAL PUBLIC LICENSE                          #
+#                       Version 3, 19 November 2007                            #
+#                                                                              #
+#    This library is a wrapper for the Microsoft Windows Defender CLI Software.#
+#    Copyright (C) 2022 Gavroche                                               #
+#                                                                              #
+#    This program is free software: you can redistribute it and/or modify      #
+#    it under the terms of the GNU Affero General Public License as published  #
+#    by the Free Software Foundation, either version 3 of the License, or      #
+#    (at your option) any later version.                                       #
+#                                                                              #
+#     This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU Affero General Public License for more details.                       #
+#                                                                              #
+#    You should have received a copy of the GNU Affero General Public License  #
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.    #
+#                                                                              #
+#                                                                              #
+################################################################################
+
+
 import os
+
+
+################################################################################
+
 
 def analyze(path, rem="-DisableRemediation"):
 
@@ -25,3 +58,12 @@ def analyze(path, rem="-DisableRemediation"):
         c = False
 
     return c
+
+
+################################################################################
+
+
+if __name__ == "__main__":
+    print('Please run main.py or read software documentation')
+
+    exit()
