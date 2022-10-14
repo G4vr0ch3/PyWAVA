b263096ffa41ab2ee957e8311a38644abe7bba10
Gavroche <arthur.reppelin@gmail.com>
2022-10-14 09:19:34 +0200

    Asterix libs

--- Asterix_libs/log.py
+++ Asterix_libs/log.py
@@ -4,27 +4,65 @@
 ################################################################################
 
 
+import subprocess
 from datetime import datetime
 
 
 ################################################################################
 
 
-def log(data):
+def init_log(path=""):
+
+    if len(path) > 0 and path[-1] != "/": path += "/"
+
+    time = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+    name = path + f'[{time}]-logfile.txt'
+
+    with open(name, "w") as logfile:
+        logfile.write(f'[{time}] - Initialized log file.')
+
+    return name
+
+
+def reset_log(file="log.txt"):
+    time = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+    with open(file, "w") as logfile:
+        logfile.write(f'[{time}] - Initialized log file.')
+    return file
+
+
+def log(data, file="log.txt"):  
 
     time = datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
 
     line = f'[{time}] - {data}'
 
-    with open("log.txt", "r+") as l_file:
+    with open(file, "r+") as l_file:
 
         logs = l_file.read()
 
-        logs = line + '\n' + logs
+        logs = logs + '\n' + line
 
         l_file.seek(0)
 
         l_file.write(logs)
+
+
+def log_from_log(local_log, global_log):
+
+    local_log_entries = open(local_log, "r").read()
+    global_log_entries = open(global_log, "r").read()
+
+    with open(global_log, "w") as gl:
+
+        entries = global_log_entries + '\n' + local_log_entries
+
+        gl.write(entries)
+
+
+def export_log(fpath):
+    subprocess.call(f'/bin/mv {fpath} /mnt/DataShare/', shell=True)
+    subprocess.call(f'/bin/chmod +r /mnt/DataShare/{fpath.split("/")[-1]}', shell=True)
 
 
 ################################################################################
